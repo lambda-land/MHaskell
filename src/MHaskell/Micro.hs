047f473195e9d4c3b8465193e84881a28417e897
Iain Moncrief <moncrief@oregonstate.edu>
2024-11-12 10:31:19 -0800

    Add data type definitions to represent MicroHaskell.

--- src/MHaskell/Micro.hs
+++ src/MHaskell/Micro.hs
@@ -1,2 +1,41 @@
 module MHaskell.Micro where
 
+
+type Name = String
+
+type Prog = [Stmt]
+
+data Stmt 
+  = SFunDef Name [Name] Expr
+  deriving (Show,Eq)
+
+
+data Expr
+  = EVar Name
+  | ELitInt Int
+  | ELitBool Bool
+  | ELitNil
+  | ELitList [Expr]
+  | EEBinOp Expr BinOp Expr
+  | EApp Expr Expr
+  | EFun Name Expr
+  | ELet Name Expr Expr
+  | EMatch Expr [CaseAlt]
+  deriving (Show,Eq)
+
+data BinOp 
+  = Add
+  | Mul
+  | Sub
+  | Div
+  | Cons
+  deriving (Show,Eq)
+
+type CaseAlt = (Pattern,Expr)
+
+data Pattern
+  = PAny
+  | PVar Name
+  | PCons Name [Pattern]
+  deriving (Show,Eq)
+
